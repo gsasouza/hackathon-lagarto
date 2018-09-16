9f73dc40e65b8f17b139caeb63220e893e70b365
jeffnts <jf.nts10@gmail.com>
2018-09-15 23:10:16 -0300

    Restore source 'build'.

--- packages/data-models-hackathon/src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as UserModel } from '../../graphql/src/user/UserModel';
-export { default as UserLoader } from '../../graphql/src/user/UserLoader';
-export { default as registerLoaders } from '../../graphql/src/registerLoaders';
-//# sourceMappingURL=index.js.map
--- packages/graphql/src/type/QueryType.ts
+++ packages/graphql/src/type/QueryType.ts
@@ -3,10 +3,13 @@
 import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID, GraphQLBoolean } from 'graphql';
 import { globalIdField, connectionArgs, fromGlobalId } from 'graphql-relay';
 
+import {EventLoader} from '../event/EventLoader';
+import {EventType} from '../modules/event/EventType';
+
 export default new GraphQLObjectType({
   name: 'Query',
   description: 'The root of all... queries',
  fields: () => ({
-
+    name
   })
 });
