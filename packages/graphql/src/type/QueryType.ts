9f93cbe707c0afbd46f0a81670855dc7a5fe15dd
jeffnts <jf.nts10@gmail.com>
2018-09-15 23:20:23 -0300

    Restore of 'build'.

--- packages/graphql/src/type/QueryType.ts
+++ packages/graphql/src/type/QueryType.ts
@@ -5,11 +5,23 @@ import { globalIdField, connectionArgs, fromGlobalId } from 'graphql-relay';
 
 import {EventLoader} from '../event/EventLoader';
 import {EventType} from '../modules/event/EventType';
+import {load} from '../mark/MarkLoader';
 
 export default new GraphQLObjectType({
   name: 'Query',
   description: 'The root of all... queries',
  fields: () => ({
-    name
+   build: {
+     type: BuildType,
+     args:{
+       id:{
+         type: new GraphQLNonNull(GraphQLID)
+       },
+       resolve:(obj, args, context) =>{
+         const {id} = fromGlobalId(args,id);
+         return UserLoad.load(context, id);
+       }
+     }
+   }
   })
 });
